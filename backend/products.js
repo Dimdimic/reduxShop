915e3a91413adbeab632bf402256776376464f8e
Dmitry <dmitrvad9@gmail.com>
2023-03-23 00:08:11 +0300

    reduxShop

---
+++ backend/products.js
@@ -0,0 +1,25 @@
+const products = [
+    {
+        id: 1,
+        name: "iPhone 12 Pro",
+        desc: "6.1-inch display",
+        price: 999,
+        image: "https://raw.githubusercontent.com/chaoocharles/complete-shopping-cart/main/backend/images/galaxyS.png",
+    },
+    {
+        id: 2,
+        name: "iPhone 12",
+        desc: "5.4-inch display",
+        price: 699,
+        image: "https://raw.githubusercontent.com/chaoocharles/complete-shopping-cart/main/backend/images/iphone12.jpg"
+    },
+    {
+        id: 3,
+        name: "Galaxy S",
+        desc: "6.5-inch display",
+        price: 399,
+        image: "https://raw.githubusercontent.com/chaoocharles/complete-shopping-cart/main/backend/images/iphone12pro.jpg"
+    },
+];
+
+module.exports = products;
